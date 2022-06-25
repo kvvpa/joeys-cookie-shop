86a05b3786926bfc358fa29a1a87a45a61ffbeb1
Joseph Cristina <kvvpa@posteo.net>
2022-06-25 16:15:39 -0700

    added confirm prompts and alerts

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-'use strict'
+"use strict";
 
 // let firstName = 'Joey';
 // let lastname = 'Cristina';
-// let numberOfDogs = 1; 
+// let numberOfDogs = 1;
 // let lovesCoffee = true;
 
 // console.log(firstName);
-
-let time = prompt('What hour is it? (0-24)?');
-let message = '';
+let userName = prompt("What is your name?");
+let time = prompt("What hour is it? (0-24)?");
+let message = "";
 
 if (time <= 11) {
-    message = 'Good Morning!';
+  message = "Good Morning, ";
 } else if (time <= 16) {
-    message = 'Good Afternoon!';
-} else if (time <= 24){
-    message = 'Good Day!';
+  message = "Good Afternoon, ";
+} else if (time <= 24) {
+  message = "Good Day, ";
 } else {
-    message = 'What are you trying to do?';
+  message = "Have a great day, ";
 }
-document.write(message);
 
+document.write(message + userName + "!");
+
+let result = confirm("Is Joey really really cool?");
+
+if (result === true) {
+  alert("Thank you");
+} else {
+  alert("Rude.");
+}
