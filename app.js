5a1e50a38e98f9a79045e7635a56e04949b15e33
Joseph Cristina <kvvpa@posteo.net>
2022-06-28 17:52:51 -0700

    used functions to encapsulate code

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
 "use strict";
 
-// let firstName = 'Joey';
-// let lastname = 'Cristina';
-// let numberOfDogs = 1;
-// let lovesCoffee = true;
-
-// console.log(firstName);
-let userName = prompt("What is your name?");
-let time = prompt("What hour is it? (0-24)?");
-let message = "";
-
-if (time <= 11) {
-  message = "Good Morning, ";
-} else if (time <= 16) {
-  message = "Good Afternoon, ";
-} else if (time <= 24) {
-  message = "Good Day, ";
-} else {
-  message = "Have a great day, ";
+function generateGreeting() {
+  let firstName = getFirstName();
+  let message = timeOfDay();
+  let isCool = isJoeyCool();
+  return document.write(message + firstName + "! " + isCool);
 }
 
-document.write(message + userName + "!");
+function getFirstName() {
+  let firstName = prompt("What is your name?");
+  return firstName;
+}
 
+function timeOfDay() {
+  let time = prompt("What hour is it? (0-24)?");
+  let message = "";
+  
+  if (time <= 11) {
+    message = "Good Morning, ";
+  } else if (time <= 16) {
+    message = "Good Afternoon, ";
+  } else if (time <= 24) {
+    message = "Good Evening, ";
+  } else {
+    message = "Have a great day, ";
+  }
+  return message;
+}
+
+function isJoeyCool() {
 let isCool = '';
+let message = '';
 isCool = confirm("Is Joey really really cool?");
 
 if (isCool === true) {
-  alert("Thank you!");
-  document.write(" You're also really really cool.")
+  // alert("Thank you!");
+message = " You're also really really cool.";
 } else {
-  alert("Rude.");
-  document.write(" You're rude.")
+  // alert("Rude.");
+  message = " You're rude.";
+}
+return message;
 }
 
-let body = document.getElementById('body');
-let color = prompt('Pick a color: orange, red, blue, or yellow');
-body.classList.add(color);
+// let body = document.getElementById('body');
+// let color = prompt('Pick a color: orange, red, blue, or yellow');
+// body.classList.add(color);
